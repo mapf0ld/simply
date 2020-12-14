28951e500eabf29ec1132b7b2b1f153e5ff0564e
mapf0ld <mapf0ld@protonmail.com>
2020-12-14 13:20:05 +0900

    DLam slightly modified

--- src/DLam.hs
+++ src/DLam.hs
@@ -19,7 +19,7 @@ data InferTerm =
     | Pi CheckTerm CheckTerm
     | Var Int
     | Par Name
-    | App InferTerm InferTerm
+    | App InferTerm CheckTerm
       deriving (Show, Eq)
 
 data Neutral =
@@ -49,7 +49,7 @@ evalInferT :: InferTerm -> Env -> Value
 evalInferT (Ann e _) env   = evalCheckT e env
 evalInferT (Par x) _       = vpar x
 evalInferT (Var i) env     = env !! i
-evalInferT (App e1 e2) env = vapp (evalInferT e1 env) (evalInferT e2 env)
+evalInferT (App e1 e2) env = vapp (evalInferT e1 env) (evalCheckT e2 env)
 evalInferT Star _          = VStar
 evalInferT (Pi t1 t2) env = VPi (evalCheckT t1 env)
                                 (\x -> evalCheckT t2 (x : env))
@@ -59,7 +59,13 @@ evalCheckT (Infer e) env = evalInferT e env
 evalCheckT (Lam t) env   = VLam (\x -> evalCheckT t (x : env))
 
 iSubst :: Int -> InferTerm -> InferTerm -> InferTerm
-iSubst i r (Ann e t) = Ann (cSubst i r e) t
+iSubst i r (Ann e t)   = Ann (cSubst i r e) t
+iSubst i r (Var j)     = if i == j then r else Var j
+iSubst _ _ (Par y)     = Par y
+iSubst i r (App e1 e2) = App (iSubst i r e1) (cSubst i r e2)
+iSubst _ _ Star        = Star
+iSubst i r (Pi t t')   = Pi (cSubst i r t) (cSubst (i + 1) r t')
 
 cSubst :: Int -> InferTerm -> CheckTerm -> CheckTerm
 cSubst i r (Infer e) = Infer (iSubst i r e)
+cSubst i r (Lam e)   = Lam (cSubst (i + 1) r e)
